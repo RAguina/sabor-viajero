26e9272ab02cd5a5f0667706b7f0ed8fb8df16ae
Rodrigo Aguinagalde <rodrigoaguinagalde@gmail.com>
2025-07-01 01:41:58 +0200

    v0.02

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 'use client';
 import React from 'react';
-import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage'; // Asegúrate de que esta ruta sea correcta
 
 export default function Page() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
-      <Navbar />
       <main className="flex-1">
         <HomePage />
       </main>
